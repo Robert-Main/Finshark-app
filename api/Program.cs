efa707a0cf2ecd8585a863072f579ec1cb31a01a
Robert-Main <mainrobert04@gmail.com>
2026-05-12 12:17:48 +0300

    initial set up and models update

---
+++ api/Program.cs
@@ -0,0 +1,63 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapGet("/swagger-test", (int? items) =>
+{
+    var count = Math.Clamp(items ?? 25, 1, 1000);
+    var results = Enumerable.Range(1, count)
+        .Select(i => new
+        {
+            id = i,
+            name = $"Swagger item {i}",
+            value = (i * 37 + 13).ToString("X")
+        })
+        .ToArray();
+
+    return Results.Ok(new
+    {
+        endpoint = "swagger-test",
+        items = count,
+        timestamp = DateTime.UtcNow,
+        results
+    });
+})
+.WithName("SwaggerTest");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
